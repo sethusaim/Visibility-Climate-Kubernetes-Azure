046563d79f6c3f4ab17a36a6ceb83ef5f416a577
sethusaim <sethusaim@gmail.com>
2022-04-27 13:07:13 +0530

    added files for data transform train

---
+++ data_transform_train/data_transformation_train.py
@@ -0,0 +1,82 @@
+from blob_operations import Blob_Operation
+from utils.logger import App_Logger
+from utils.read_params import read_params
+
+
+class Data_Transform_Train:
+    """
+    Description :  This class shall be used for transforming the trainiction batch data before loading it in Database!!.
+
+    Version     :   1.2
+    Revisions   :   None
+    """
+
+    def __init__(self):
+        self.config = read_params()
+
+        self.container = self.config["blob_container"]
+
+        self.blob = Blob_Operation()
+
+        self.log_writer = App_Logger()
+
+        self.data_dir = self.config["data_dir"]
+
+        self.class_name = self.__class__.__name__
+
+        self.train_data_transform_log = self.config["log"]["data_transform"]
+
+    def add_quotes_to_string(self):
+        """
+        Method Name :   add_quotes_to_string
+        Description :   This method addes the quotes to the string data present in columns
+
+        Version     :   1.2
+        Revisions   :   moved setup to cloud
+        """
+        method_name = self.add_quotes_to_string.__name__
+
+        self.log_writer.start_log(
+            "start", self.class_name, method_name, self.train_data_transform_log
+        )
+
+        try:
+            lst = self.blob.read_csv_from_folder(
+                self.data_dir["good"],
+                self.container["train_data"],
+                self.train_data_transform_log,
+            )
+
+            for idx, f in enumerate(lst):
+                df = f[idx][0]
+
+                file = f[idx][1]
+
+                abs_f = f[idx][2]
+
+                df["DATE"] = df["DATE"].apply(lambda x: "'" + str(x) + "'")
+
+                self.log_writer.log(
+                    f"Quotes added for the file {file}", self.train_data_transform_log
+                )
+
+                self.blob.upload_df_as_csv(
+                    df,
+                    abs_f,
+                    file,
+                    self.container["train_data"],
+                    self.train_data_transform_log,
+                )
+
+            self.log_writer.start_log(
+                key="exit", class_name=self.class_name, method_name=method_name,
+            )
+
+            self.log_writer.start_log(
+                "exit", self.class_name, method_name, self.train_data_transform_log
+            )
+
+        except Exception as e:
+            self.log_writer.exception_log(
+                e, self.class_name, method_name, self.train_data_transform_log
+            )
---
+++ data_transform_train/run.py
@@ -0,0 +1,51 @@
+from data_transformation_train import Data_Transform_Train
+from utils.logger import App_Logger
+from utils.main_utils import Main_Utils
+from utils.read_params import read_params
+
+
+class Run:
+    def __init__(self):
+        self.config = read_params()
+
+        self.class_name = self.__class__.__name__
+
+        self.train_main_log = self.config["log"]["data_transform_main"]
+
+        self.log_writer = App_Logger()
+
+        self.data_transform = Data_Transform_Train()
+
+    def train_data_transform(self):
+        method_name = self.train_data_transform.__name__
+
+        try:
+            self.log_writer.log("Starting Data Transformation", self.train_main_log)
+
+            self.data_transform.add_quotes_to_string()
+
+            self.log_writer.log("Data Transformation completed !!", self.train_main_log)
+
+            self.log_writer.start_log(
+                "exit", self.class_name, method_name, self.train_main_log
+            )
+
+        except Exception as e:
+            self.log_writer.exception_log(
+                e, self.class_name, method_name, self.train_main_log,
+            )
+
+
+if __name__ == "__main__":
+    try:
+        run = Run()
+
+        run.train_data_transform()
+
+    except Exception as e:
+        raise e
+
+    finally:
+        utils = Main_Utils()
+
+        utils.upload_logs()
